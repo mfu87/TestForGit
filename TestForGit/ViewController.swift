d1d23ff1e0cc079c086cfe283483d64aa759ab16
Marcus Fuchs <mfuchs@mf-MacBookAir.fritz.box>
2019-06-26 16:44:37 +0200

    Initial Commit

---
+++ TestForGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestForGit
+//
+//  Created by Marcus Fuchs on 26.06.19.
+//  Copyright © 2019 Marcus Fuchs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
